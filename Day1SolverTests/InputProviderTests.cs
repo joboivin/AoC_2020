3a19c5bbef5f9eb1937932f422b8a8e11a669d6e
joboivin <jobr_guitare@hotmail.com>
2020-12-01 19:26:15 -0500

    Add test to make sure all lines are read

---
+++ Day1SolverTests/InputProviderTests.cs
@@ -0,0 +1,19 @@
+﻿using Day1Solver;
+using FluentAssertions;
+using Xunit;
+
+namespace Day1SolverTests
+{
+    public class InputProviderTests
+    {
+        [Fact]
+        public void ProvideInput_ThenReturns200Numbers()
+        {
+            var subject = new InputProvider();
+
+            var result = subject.ProvideInput();
+
+            result.Should().HaveCount(200, "there are 200 lines in the input file");
+        }
+    }
+}
